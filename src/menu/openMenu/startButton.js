a5d6eec3ff981b93ee67605f5a4c211b09fbf364
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-04-05 02:44:52 -0500

    1

--- src/menu/openMenu/startButton.js
+++ src/menu/openMenu/startButton.js
@@ -4,35 +4,34 @@ import {
   START_BUTTON_WRAPPER_CLASS,
 } from "../menuConstants";
 
+
 export class StartButton {
   constructor(element) {
     this.ready(element);
   }
   ready(element) {
-    const newButton = document.createElement("button");
-    newButton.classList.add(START_BUTTON_CLASS);
-    newButton.innerText = START_BUTTON_TEXT;
-
-    const newButtonWrapper = document.createElement("div");
-    newButtonWrapper.classList.add(START_BUTTON_WRAPPER_CLASS);
-    newButtonWrapper.appendChild(newButton);
-
-    element.appendChild(newButtonWrapper);
-
-    newButton.addEventListener("click", () => {
-      const response = window.menuAPI.openMenu();
-
-      console.log("Start button was handled:", response);
-
-      if (response) {
-        remove();
-      }
-    });
+    addTo(element);
   }
 }
-function remove() {
-  const elementToRemove = document.querySelectorAll(
-    `.${START_BUTTON_WRAPPER_CLASS}`
-  );
-  elementToRemove[0].remove();
+
+function addTo(element) {
+  const newButton = document.createElement("button");
+  newButton.classList.add(START_BUTTON_CLASS);
+  newButton.innerText = START_BUTTON_TEXT;
+
+  const newButtonWrapper = document.createElement("div");
+  newButtonWrapper.classList.add(START_BUTTON_WRAPPER_CLASS);
+  newButtonWrapper.appendChild(newButton);
+
+  element.appendChild(newButtonWrapper);
+
+  newButton.addEventListener("click", () => {
+    const response = window.menuAPI.openMenu();
+
+    console.log("Start button was handled:", response);
+
+    if (!!response) {
+      newButtonWrapper.remove();
+    }
+  });
 }
