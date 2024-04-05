38421a6ff157ce349402cc3a1d30979735deab07
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-04-05 10:41:11 -0500

    1

--- src/GameMenu/openGameMenu/initOpenGameMenu.js
+++ src/GameMenu/openGameMenu/initOpenGameMenu.js
@@ -3,11 +3,16 @@ import { openGameMenu } from "./openGameMenu";
 
 export function initOpenGameMenu() {
   ipcMain.on("openGameMenu:launch-button", async (event) => {
+    // event recieved from the renderer
+
+    // enable the menu api
     openGameMenu();
 
+    // alert the renderer to display the menu, the api is listening
     event.returnValue = true;
 
     console.log("[main] Tell the renderer to open the game menu.");
   });
+  //  return true to the module that the listener was loaded
   return true;
 }
