28f5df12f8e7342ca010248b3600a550c236989f
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-04-05 14:19:45 -0500

    1

--- src/GameMenu/gameMenuMain/launch/initLaunchSequence.js
+++ src/GameMenu/gameMenuMain/launch/initLaunchSequence.js
@@ -1,11 +1,12 @@
 import { ipcMain } from "electron";
-
+import { launchSequence } from "./launchSequence";
 export function initLaunchSequence() {
   ipcMain.on("game-menu:launch", async (event) => {
-
+    launchSequence();
     event.returnValue = { options: "" };
     console.log("[main] Tell the renderer to launch.");
   });
   //  return true to the module that the listener was loaded
   return true;
 }
+
